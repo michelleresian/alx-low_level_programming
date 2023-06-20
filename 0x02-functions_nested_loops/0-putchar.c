bbfdd4d06af43696cfcd04b27cc96310d5b2d3cd
michelleresian <michellresian@gmail.com>
2023-06-20 15:16:47 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point
+ * description: 'prints _putchar to stdout'
+ * Return: (0) always a success
+ */
+int main(void)
+{
+	_putchar('_')
+	_putchar('p')
+	_putchar('u')
+	_putchar('t')
+	_putchar('c')
+	_putchar('h')
+	_putchar('a')
+	_putchar('r')
+	return (0)
+}
