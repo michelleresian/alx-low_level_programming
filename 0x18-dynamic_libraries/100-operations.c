5f45938d668f2ff3dab9074eb3e18e0ebce9f48c
michelleresian <michellresian@gmail.com>
2023-10-20 16:51:25 -0700

    task 100

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+/**
+ * add - adds two integers
+ * @a: first int
+ * @b: second int
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subs two integers
+ * @a: first int
+ * @b: second int
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplies two integers
+ * @a: first int
+ * @b: second int
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divs two integers
+ * @a: first int
+ * @b: second int
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - mods two integers
+ * @a: first int
+ * @b: second int
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
